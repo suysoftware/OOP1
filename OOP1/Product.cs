0eb96b225aaf260b39a3ca259d9288704e604eb7
suysoftware <78134326+suysoftware@users.noreply.github.com>
2021-07-05 18:52:59 +0300

    Add project files.

---
+++ OOP1/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+
+        //snippet = hazır kodlar örneğin proptabtab
+
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ProdcutName { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int UnitInStock { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {   
+        //PascalCase=Kelimelerin ilk harfinin büyük yazılması
+        //camelCase = ilk harfi küçük diğerleri büyük
+        //case sensitive= büüyk küçük harf duyarlı
+
+        static void Main(string[] args)
+        {
+
+
+            //1.YONTEM
+
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProdcutName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+
+
+            //2.YONTEM
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitInStock = 5, UnitPrice = 35, ProdcutName = "Kalem" };
+
+
+            //3.YONTEM
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+
+
+
+        }
+    }
+}
